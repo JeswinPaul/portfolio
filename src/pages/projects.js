6763996e76a99584dc85a3714562d7d6323c03a5
JeswinPaul <jeswinpaul1@gmail.com>
2024-02-12 11:31:23 +0530

    ok

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,14 +7,14 @@ export default function Projects() {
         <span>
             {data?.map((item, index)=>{
               return(
-                <div className='Projects_Con'>
+                <a href={item?.url} target='_blank' className='Projects_Con'>
                   <img key={index} src={item?.img}/>
                   <h3>{item?.title}</h3>
                   <p>My Role: {item?.role}</p>
                   <div className='Projects_Con_Badge'>
                     {item?.type || "Website"}
                   </div>
-                </div>
+                </a>
               )
             })}
         </span>
@@ -24,22 +24,22 @@ export default function Projects() {
 }
 
 const data = [
-  {img:require('../assets/1.webp'), url:"", title:"Inovador TechSolutions", role:"Front-End developer"},
-  {img:require('../assets/2.webp'), url:"", title:"Plan B Dental Clinic",role:"Front-End developer"},
-  {img:require('../assets/3.webp'), url:"", title:"PE Fitness",role:"Front-End developer"},
-  {img:require('../assets/5.webp'), url:"", title:"Backgen.io", role:"Full stack developer", type:"Web App"},
-  {img:require('../assets/6.webp'), url:"", title:"Maya Mask",role:"Front-End developer"},
-  {img:require('../assets/7.webp'), url:"", title:"Zybertone",role:"Front-End developer"},
-  {img:require('../assets/8.webp'), url:"", title:"Evermore",role:"Front-End developer"},
-  {img:require('../assets/9.webp'), url:"", title:"Farming Underdogs",role:"Front-End developer"},
-  {img:require('../assets/10.webp'), url:"", title:"Love Llamas", role:"Front-End developer"},
-  {img:require('../assets/11.webp'), url:"", title:"Zlize",role:"Full stack developer", type:"Web App"},
-  {img:require('../assets/12.webp'), url:"", title:"Finncare Travel",role:"Full stack developer", type:"Web App"},
-  {img:require('../assets/13.webp'), url:"", title:"Unstopable Blue Eagles",role:"Front-End developer"},
-  {img:require('../assets/14.webp'), url:"", title:"Xand",role:"Full stack developer", type:"Web App"},
-  {img:require('../assets/15.webp'), url:"", title:"Zwink",role:"Full stack developer", type:"Web App"},
-  {img:require('../assets/4.webp'), url:"", title:"HDS", role:"Front-End developer"},
-  {img:require('../assets/17.webp'), url:"", title:"YFC Event App", role:"Full stack developer", type:"Mobile"},
-  {img:require('../assets/18.webp'), url:"", title:"Zwink Education App", role:"Full stack developer", type:"Mobile"},
-  {img:require('../assets/19.webp'), url:"", title:"Saturate", role:"Full stack developer", type:"Mobile"},
+  {img:require('../assets/1.webp'), url:"https://inovadortechsolutions.com", title:"Inovador TechSolutions", role:"Front-End developer"},
+  {img:require('../assets/2.webp'), url:"https://www.planbdentalclinic.com/home", title:"🚧 Plan B Dental Clinic 🚧",role:"Front-End developer"},
+  {img:require('../assets/3.webp'), url:"https://pefitness.in/home", title:"🚧 PE Fitness 🚧",role:"Front-End developer"},
+  {img:require('../assets/5.webp'), url:"https://backgen.io", title:"Backgen.io", role:"Full stack developer", type:"Web App"},
+  {img:require('../assets/6.webp'), url:"https://mayaprotocol.vercel.app", title:"Maya Mask",role:"Front-End developer"},
+  {img:require('../assets/7.webp'), url:"https://zybertone.com", title:"Zybertone",role:"Front-End developer"},
+  {img:require('../assets/8.webp'), url:"https://evermore-omega.vercel.app/", title:"Evermore",role:"Front-End developer"},
+  {img:require('../assets/9.webp'), url:"https://farmingunderdogs.com", title:"Farming Underdogs",role:"Front-End developer"},
+  {img:require('../assets/10.webp'), url:"https://lovellamas.io", title:"Love Llamas", role:"Front-End developer"},
+  {img:require('../assets/11.webp'), url:"https://zlize.vercel.app", title:"Zlize",role:"Full stack developer", type:"Web App"},
+  {img:require('../assets/12.webp'), url:"https://finncare-travel.com", title:"Finncare Travel",role:"Full stack developer", type:"Web App"},
+  {img:require('../assets/13.webp'), url:"https://ube.app", title:"Unstopable Blue Eagles",role:"Front-End developer"},
+  {img:require('../assets/14.webp'), url:"https://xand.app", title:"Xand",role:"Full stack developer", type:"Web App"},
+  {img:require('../assets/15.webp'), url:"https://steponee.vercel.app/", title:"Zwink",role:"Full stack developer", type:"Web App"},
+  {img:require('../assets/4.webp'), url:"https://hds-site.vercel.app/", title:"HDS", role:"Front-End developer"},
+  {img:require('../assets/17.webp'), url:"https://play.google.com/store/apps/details?id=org.yfci.connect", title:"YFC Event App", role:"Full stack developer", type:"Mobile"},
+  {img:require('../assets/18.webp'), url:"https://play.google.com/store/apps/details?id=in.zwink.app", title:"Zwink Education App", role:"Full stack developer", type:"Mobile"},
+  {img:require('../assets/19.webp'), url:"https://play.google.com/store/apps/details?id=com.mykeystone.yfcsaturate", title:"Saturate", role:"Full stack developer", type:"Mobile"},
 ]
